4690e378005a380aa0144ceb3d948f932bba25af
Miguel Duarte <52736640+trapflames@users.noreply.github.com>
2022-12-06 22:32:31 +0000

    Day 6

---
+++ 6/b.ts
@@ -0,0 +1,26 @@
+const input = await Deno.readTextFile('./input.txt')
+const data = input.split('')
+
+const BUFFER_SIZE = 14
+let charMap = data.slice(0, BUFFER_SIZE)
+let charSet = [...new Set(charMap)]
+let startOfMessage = 0
+
+const result = () => {
+  for (let charIndex = 1; charIndex < data.length - BUFFER_SIZE; charIndex++) {
+    if (charSet.length === BUFFER_SIZE) {
+      startOfMessage = charIndex + BUFFER_SIZE - 1
+      break
+    }
+    const start = charIndex
+    const end = start + BUFFER_SIZE
+
+    charMap = data.slice(start, end)
+    charSet = [...new Set(charMap)]
+  }
+  return `The first start-of-packet marker is ${startOfMessage} ${
+    startOfMessage === 3495 ? '✅' : '❌'
+  }`
+}
+
+console.log(result())
